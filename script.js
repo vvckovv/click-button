a81bfd0a45521d64626acb74a064328c6350ff2f
vvckovv <oloyedeope14@gmail.com>
2024-06-20 18:39:02 +0100

    add text on the shape

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const shape = document.getElementById('shape');
 const greenButton = document.getElementById('greenBtn');
 const redButton = document.getElementById('redBtn');
 
-function changeColorAndShape(color, isCircle) {
-shape.style.backgroundColor = color;
+function changeColorAndShape(color, text, isCircle) {
+  shape.style.backgroundColor = color;
+  shape.textContent = text;
   if(isCircle) {
     shape.style.borderRadius = '50%';
     shape.style.width = '100px';
@@ -15,6 +16,6 @@ shape.style.backgroundColor = color;
   }
 }
 
-greenBtn.addEventListener('click', () => changeColorAndShape('green', true));
-redBtn.addEventListener('click', () => changeColorAndShape('red',false));
+greenBtn.addEventListener('click', () => changeColorAndShape('green', greenBtn.textContent, true));
+redBtn.addEventListener('click', () => changeColorAndShape('red', redBtn.textContent, false));
 resetBtn.addEventListener('click', () => changeColorAndShape('black'));
