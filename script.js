d7efb0557f992fdced6fbd589bb0ef412260e225
vvckovv <oloyedeope14@gmail.com>
2024-06-20 10:27:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const rectangle = document.getElementById('rectangle');
+const greenButton = document.getElementById('greenBtn');
+const redButton = document.getElementById('redBtn');
+
+function changeColor(color) {
+rectangle.style.backgroundColor = color;
+}
+
+greenBtn.addEventListener('click', () => changeColor('green'));
+redBtn.addEventListener('click', () => changeColor('red'));
