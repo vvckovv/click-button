0eb6bb26c6a2fefa00e49dc25f3fef40fbcd629d
vvckovv <oloyedeope14@gmail.com>
2024-06-20 15:31:55 +0100

    changed shape

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const rectangle = document.getElementById('rectangle');
+const shape = document.getElementById('shape');
 const greenButton = document.getElementById('greenBtn');
 const redButton = document.getElementById('redBtn');
 
-function changeColor(color) {
-rectangle.style.backgroundColor = color;
+function changeColorAndShape(color, isCircle) {
+shape.style.backgroundColor = color;
+  if(isCircle) {
+    shape.style.borderRadius = '50%';
+    shape.style.width = '100px';
+    shape.style.height = '100px';
+  } else {
+    shape.style.borderRadius = '0';
+    shape.style.width = '200px';
+    shape.style.height = '100px';
+  }
 }
 
-greenBtn.addEventListener('click', () => changeColor('green'));
-redBtn.addEventListener('click', () => changeColor('red'));
-resetBtn.addEventListener('click', () => changeColor('black'));
+greenBtn.addEventListener('click', () => changeColorAndShape('green', true));
+redBtn.addEventListener('click', () => changeColorAndShape('red',false));
+resetBtn.addEventListener('click', () => changeColorAndShape('black'));
